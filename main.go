df9aa353439fbe8537f21544cb0f30029ae05647
Tan90degrees <72683500+Tan90degrees@users.noreply.github.com>
2023-05-29 23:02:24 +0800

    Add : ability to compare files.

--- main.go
+++ main.go
@@ -4,86 +4,75 @@ import (
 	"checksum/myhash"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strings"
-	"sync"
 	"time"
 )
 
 func main() {
 	timeStart := time.Now()
-	path := flag.String("p", "", "Path to check.")
-	way := flag.String("w", "", "The way to check.\nPlease choose between SHA1, SHA256, SHA512, MD5 or ALL.")
-	sum := flag.String("s", "", "Checksum should be.")
+
+	src := flag.String("src", "", "Path to check.")
+	dst := flag.String("dst", "", "Path to compare with.")
+	way := flag.String("way", "", "The way to check.\nPlease choose between SHA1, SHA256, SHA512, MD5 or ALL.")
+	sum := flag.String("sum", "", "Checksum should be.")
+
 	flag.Parse()
-	var cFlag bool = true
-	var isRight bool
-	if *sum == "" {
-		cFlag = false
+
+	checker := &myhash.Checker{Path: *src}
+
+	if *sum != "" {
+		checker.CheckSum = strings.ToUpper(*sum)
 	}
-	checker := myhash.Checker{Path: *path, Sum: strings.ToUpper(*sum)}
+
 	switch *way {
-	case "SHA1":
-		isRight = myhash.Sha1(&checker, cFlag)
-	case "sha1":
-		isRight = myhash.Sha1(&checker, cFlag)
-	case "SHA256":
-		isRight = myhash.Sha256(&checker, cFlag)
-	case "sha256":
-		isRight = myhash.Sha256(&checker, cFlag)
-	case "SHA512":
-		isRight = myhash.Sha512(&checker, cFlag)
-	case "sha512":
-		isRight = myhash.Sha512(&checker, cFlag)
-	case "MD5":
-		isRight = myhash.Md5(&checker, cFlag)
-	case "md5":
-		isRight = myhash.Md5(&checker, cFlag)
-	case "ALL":
-		var wg sync.WaitGroup
-		mychan := make(chan string)
-		wg.Add(4)
-		go myhash.MSha256(&checker, &wg, &mychan)
-		go myhash.MSha512(&checker, &wg, &mychan)
-		go myhash.MMd5(&checker, &wg, &mychan)
-		go myhash.MSha1(&checker, &wg, &mychan)
-		for i := 0; i < 4; i++ {
-			fmt.Print(<-mychan)
-		}
-		wg.Wait()
-		close(mychan)
-		fmt.Println("Time:", time.Since(timeStart))
-		os.Exit(0)
-	case "all":
-		var wg sync.WaitGroup
-		mychan := make(chan string)
-		wg.Add(4)
-		go myhash.MSha256(&checker, &wg, &mychan)
-		go myhash.MSha512(&checker, &wg, &mychan)
-		go myhash.MMd5(&checker, &wg, &mychan)
-		go myhash.MSha1(&checker, &wg, &mychan)
-		for i := 0; i < 4; i++ {
-			fmt.Print(<-mychan)
-		}
-		wg.Wait()
-		close(mychan)
-		fmt.Println("Time:", time.Since(timeStart))
-		os.Exit(0)
+	case "sha1", "SHA1":
+		checker.Flag = myhash.CHECK_SUM_SHA1
+
+	case "sha256", "SHA256":
+		checker.Flag = myhash.CHECK_SUM_SHA256
+
+	case "sha512", "SHA512":
+		checker.Flag = myhash.CHECK_SUM_SHA512
+
+	case "md5", "MD5":
+		checker.Flag = myhash.CHECK_SUM_MD5
+
+	case "all", "ALL":
+		checker.Flag = myhash.CHECK_SUM_ALL
+
 	default:
 		fmt.Println("Please choose between SHA1, SHA256, SHA512, MD5 or ALL.")
 		os.Exit(0)
 	}
-	if cFlag {
-		if isRight {
-			fmt.Println("Right!!!")
-			fmt.Println("Time:", time.Since(timeStart))
-			os.Exit(0)
-		} else {
-			fmt.Println("False!!!")
-			fmt.Println("Time:", time.Since(timeStart))
-			os.Exit(0)
+
+	err := checker.Sum()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(checker)
+
+	var dstChecker *myhash.Checker
+
+	if *dst != "" {
+		dstChecker = &myhash.Checker{Path: *dst, Flag: checker.Flag}
+		err = dstChecker.Sum()
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
-	fmt.Println("Time:", time.Since(timeStart))
+
+	if *sum != "" || *dst != "" {
+		checker.IsCorrect = myhash.CompareCheckSum(dstChecker, checker, checker.CheckSum)
+		if checker.IsCorrect {
+			fmt.Print("\nCompare result: Right!!!\n\n")
+		} else {
+			fmt.Print("\nCompare result: False!!!\n\n")
+		}
+	}
+
+	fmt.Println("Time used:", time.Since(timeStart))
 	os.Exit(0)
 }
--- myhash/hash.go
+++ myhash/hash.go
@@ -9,105 +9,182 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
+)
+
+const (
+	CHECK_SUM_SHA1 = iota
+	CHECK_SUM_SHA256
+	CHECK_SUM_SHA512
+	CHECK_SUM_MD5
+	CHECK_SUM_ALL
 )
 
 type Checker struct {
-	Path string
-	Sum  string
+	Path      string
+	CheckSum  string
+	IsCorrect bool
+	Flag      int
+	Size      int64
+	SumsUp    [CHECK_SUM_ALL]string
+	SumsLow   [CHECK_SUM_ALL]string
 }
 
-func Sha1(checker *Checker, cFlag bool) bool {
-	fp, err := os.Open(checker.Path)
+func (c *Checker) Sum() error {
+	var wg sync.WaitGroup
+	fi, err := os.Stat(c.Path)
+	if err != nil {
+		return err
+	}
+	c.Size = fi.Size()
+	switch c.Flag {
+	case CHECK_SUM_SHA1:
+		wg.Add(1)
+		go c.Sha1(&wg)
+	case CHECK_SUM_SHA256:
+		wg.Add(1)
+		go c.Sha256(&wg)
+	case CHECK_SUM_SHA512:
+		wg.Add(1)
+		go c.Sha512(&wg)
+	case CHECK_SUM_MD5:
+		wg.Add(1)
+		go c.Md5(&wg)
+	case CHECK_SUM_ALL:
+		wg.Add(CHECK_SUM_ALL)
+		go c.Sha1(&wg)
+		go c.Sha256(&wg)
+		go c.Sha512(&wg)
+		go c.Md5(&wg)
+	default:
+		return fmt.Errorf("bad flag : %d", c.Flag)
+	}
+	wg.Wait()
+	return nil
+}
+
+func (c *Checker) Sha1(wg *sync.WaitGroup) {
+	fp, err := os.Open(c.Path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer fp.Close()
 	hasher := sha1.New()
-	size, err := io.Copy(hasher, fp)
+	_, err = io.Copy(hasher, fp)
 	if err != nil {
 		log.Fatal(err)
 	}
-	sumUp := fmt.Sprintf("%X", hasher.Sum(nil))
-	sumLow := fmt.Sprintf("%x", hasher.Sum(nil))
-	fmt.Printf("Size : %d bytes\nSHA1 : %s\nsha1 : %s\n\n", size, sumUp, sumLow)
-	if cFlag {
-		if sumUp == checker.Sum {
-			return true
-		} else {
-			return false
-		}
-	}
-	return true
+
+	c.SumsUp[CHECK_SUM_SHA1] = fmt.Sprintf("%X", hasher.Sum(nil))
+	c.SumsLow[CHECK_SUM_SHA1] = fmt.Sprintf("%x", hasher.Sum(nil))
+
+	wg.Done()
 }
 
-func Sha256(checker *Checker, cFlag bool) bool {
-	fp, err := os.Open(checker.Path)
+func (c *Checker) Sha256(wg *sync.WaitGroup) {
+	fp, err := os.Open(c.Path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer fp.Close()
 	hasher := sha256.New()
-	size, err := io.Copy(hasher, fp)
+	_, err = io.Copy(hasher, fp)
 	if err != nil {
 		log.Fatal(err)
 	}
-	sumUp := fmt.Sprintf("%X", hasher.Sum(nil))
-	sumLow := fmt.Sprintf("%x", hasher.Sum(nil))
-	fmt.Printf("Size : %d bytes\nSHA256 : %s\nsha256 : %s\n\n", size, sumUp, sumLow)
-	if cFlag {
-		if sumUp == checker.Sum {
-			return true
-		} else {
-			return false
-		}
-	}
-	return true
+
+	c.SumsUp[CHECK_SUM_SHA256] = fmt.Sprintf("%X", hasher.Sum(nil))
+	c.SumsLow[CHECK_SUM_SHA256] = fmt.Sprintf("%x", hasher.Sum(nil))
+
+	wg.Done()
 }
 
-func Sha512(checker *Checker, cFlag bool) bool {
-	fp, err := os.Open(checker.Path)
+func (c *Checker) Sha512(wg *sync.WaitGroup) {
+	fp, err := os.Open(c.Path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer fp.Close()
 	hasher := sha512.New()
-	size, err := io.Copy(hasher, fp)
+	_, err = io.Copy(hasher, fp)
 	if err != nil {
 		log.Fatal(err)
 	}
-	sumUp := fmt.Sprintf("%X", hasher.Sum(nil))
-	sumLow := fmt.Sprintf("%x", hasher.Sum(nil))
-	fmt.Printf("Size : %d bytes\nSHA512 : %s\nsha512 : %s\n\n", size, sumUp, sumLow)
-	if cFlag {
-		if sumUp == checker.Sum {
-			return true
-		} else {
-			return false
-		}
-	}
-	return true
+
+	c.SumsUp[CHECK_SUM_SHA512] = fmt.Sprintf("%X", hasher.Sum(nil))
+	c.SumsLow[CHECK_SUM_SHA512] = fmt.Sprintf("%x", hasher.Sum(nil))
+
+	wg.Done()
 }
 
-func Md5(checker *Checker, cFlag bool) bool {
-	fp, err := os.Open(checker.Path)
+func (c *Checker) Md5(wg *sync.WaitGroup) {
+	fp, err := os.Open(c.Path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer fp.Close()
 	hasher := md5.New()
-	size, err := io.Copy(hasher, fp)
+	_, err = io.Copy(hasher, fp)
 	if err != nil {
 		log.Fatal(err)
 	}
-	sumUp := fmt.Sprintf("%X", hasher.Sum(nil))
-	sumLow := fmt.Sprintf("%x", hasher.Sum(nil))
-	fmt.Printf("Size : %d bytes\nMD5 : %s\nmd5 : %s\n\n", size, sumUp, sumLow)
-	if cFlag {
-		if sumUp == checker.Sum {
-			return true
-		} else {
-			return false
-		}
+
+	c.SumsUp[CHECK_SUM_MD5] = fmt.Sprintf("%X", hasher.Sum(nil))
+	c.SumsLow[CHECK_SUM_MD5] = fmt.Sprintf("%x", hasher.Sum(nil))
+
+	wg.Done()
+}
+
+func (c *Checker) String() string {
+	var res string
+	res = fmt.Sprintf("File(%s) checksum result:\n", c.Path)
+	switch c.Flag {
+	case CHECK_SUM_SHA1:
+		res += fmt.Sprintf("  Size : %d bytes\n  SHA1 : %s\n  sha1 : %s", c.Size, c.SumsUp[CHECK_SUM_SHA1], c.SumsLow[CHECK_SUM_SHA1])
+	case CHECK_SUM_SHA256:
+		res += fmt.Sprintf("  Size : %d bytes\n  SHA256 : %s\n  sha256 : %s", c.Size, c.SumsUp[CHECK_SUM_SHA256], c.SumsLow[CHECK_SUM_SHA256])
+	case CHECK_SUM_SHA512:
+		res += fmt.Sprintf("  Size : %d bytes\n  SHA512 : %s\n  sha512 : %s", c.Size, c.SumsUp[CHECK_SUM_SHA512], c.SumsLow[CHECK_SUM_SHA512])
+	case CHECK_SUM_MD5:
+		res += fmt.Sprintf("  Size : %d bytes\n  MD5 : %s\n  md5 : %s", c.Size, c.SumsUp[CHECK_SUM_MD5], c.SumsLow[CHECK_SUM_MD5])
+	case CHECK_SUM_ALL:
+		res += fmt.Sprintf("  Size : %d bytes\n  SHA1 : %s\n  sha1 : %s\n", c.Size, c.SumsUp[CHECK_SUM_SHA1], c.SumsLow[CHECK_SUM_SHA1])
+		res += fmt.Sprintf("  SHA256 : %s\n  sha256 : %s\n", c.SumsUp[CHECK_SUM_SHA256], c.SumsLow[CHECK_SUM_SHA256])
+		res += fmt.Sprintf("  SHA512 : %s\n  sha512 : %s\n", c.SumsUp[CHECK_SUM_SHA512], c.SumsLow[CHECK_SUM_SHA512])
+		res += fmt.Sprintf("  MD5 : %s\n  md5 : %s", c.SumsUp[CHECK_SUM_MD5], c.SumsLow[CHECK_SUM_MD5])
+	default:
+		res = "Empty"
 	}
-	return true
+
+	return res
+}
+
+func CompareCheckSum(dst *Checker, src *Checker, sum string) bool {
+	res := false
+
+	if sum != "" {
+		for i := 0; i < CHECK_SUM_ALL; i++ {
+			if src.SumsUp[i] == sum {
+				res = true
+			}
+		}
+	} else {
+		res = true
+	}
+
+	if !res || dst == nil {
+		return res
+	}
+
+	if dst.Flag != CHECK_SUM_ALL {
+		return dst.SumsUp[dst.Flag] == src.SumsUp[dst.Flag]
+	} else {
+		for i := 0; i < CHECK_SUM_ALL; i++ {
+			if dst.SumsUp[i] != src.SumsUp[i] {
+				fmt.Printf("Dst: name(%s), sum(%s), size(%d)\nSrc: name(%s), sum(%s), size(%d)\n", dst.Path, dst.SumsUp[i], dst.Size, src.Path, src.SumsUp[i], src.Size)
+				return false
+			}
+		}
+		return true
+	}
 }
